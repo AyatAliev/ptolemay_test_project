b906836fd311d777e631d5fd7e955b0b38937edd
nooken <nooken@deveem.io>
2022-06-17 23:03:02 +0600

    main: Change class name

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ import 'package:ptolemay_test_project/routes.dart';
 
 Future<void> main() async {
   await di();
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
